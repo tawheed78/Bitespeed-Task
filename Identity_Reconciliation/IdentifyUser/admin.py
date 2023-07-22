511e7e93905b06cca33900ede4b6248a29c0c96a
Tawheed <tawheed.chilwan55@gmail.com>
2023-07-23 03:12:56 +0530

    Displayed createdAt and deletedAt in the database Table

--- Identity_Reconciliation/IdentifyUser/admin.py
+++ Identity_Reconciliation/IdentifyUser/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Contact
 
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'phoneNumber', 'email', 'linkedId', 'linkPrecedence', 'createdAt', 'updatedAt', 'deletedAt' )
 
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
