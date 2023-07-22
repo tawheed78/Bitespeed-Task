b2434ee92f9fb9c231c7ed81309124e394d7b779
Tawheed <tawheed.chilwan55@gmail.com>
2023-07-23 02:00:32 +0530

    added additional code after debugging  as prev one was only able to create user but not able to handle the duplicate one

--- Identity_Reconciliation/IdentifyUser/models.py
+++ Identity_Reconciliation/IdentifyUser/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Contact(models.Model):
-    phone_number = models.CharField(max_length=10, unique=True)
-    email = models.EmailField(unique=True)
+    phoneNumber = models.CharField(max_length=10, default='', unique=False)
+    email = models.CharField(max_length=100, unique=False)
     linkedId = models.ForeignKey('self',null=True, blank=True, on_delete=models.SET_NULL)
-    link_precedence = models.CharField(max_length=10, default='primary')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(aut0_now = True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
+    linkPrecedence = models.CharField(max_length=10, default='primary')
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now = True)
+    deletedAt = models.DateTimeField(null=True, blank=True)
 
 def __str__(self):
-    return self.email or self.phone_number
+    return self.email or self.phoneNumber
--- Identity_Reconciliation/IdentifyUser/views.py
+++ Identity_Reconciliation/IdentifyUser/views.py
@@ -1,38 +1,123 @@
-from typing import List
-from urllib import response
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from .models import Contact
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def identify_user(request):
+
     if request.method == 'POST':
         data = json.loads(request.body)
         email = data.get('email')
-        phone_number = data.get('phone_number')
+        phoneNumber = data.get('phoneNumber')
 
         try:
-            primary_contact = Contact.objects.get(email=email) or Contact.objects.get(phone_number=phone_number)
-        except:
-            Contact.objects.create(email=email, phone_number=phone_number, link_precedence='primary')
+            primary_contact_email = list(Contact.objects.filter(email=email).all())
 
-        if primary_contact.link_precedence == 'primary':
-            primary_contact.link_precedence = 'secondary'
-            primary_contact.save()
-        
-        secondary_contact = Contact.objects.create(email=email, phone_number=phone_number, link_precedence='primary', linkedId = primary_contact.id)
-        secondary_contact.save()
+        except Contact.DoesNotExist:
+            primary_contact_email = []
+        try:
+            primary_contact_phone = list(Contact.objects.filter(phoneNumber=phoneNumber).all())
+        except Contact.DoesNotExist:
+            primary_contact_phone = []
 
-        response_data = {
+        print(primary_contact_email)
+        print(primary_contact_phone)
+
+        if len(primary_contact_email)==0 and len(primary_contact_phone)==0:
+            primary_contact = Contact.objects.create(email=email, phoneNumber=phoneNumber, linkPrecedence='primary')
+            
+            print("created", primary_contact)
+            response_data = {
             "contact": {
                 "primaryContactId": primary_contact.id,
-                "emails": List[email],
-                "phone_number": List[phone_number],
-                "secondaryContactIds": secondary_contact.id
+                "emails": [primary_contact.email],
+                "phoneNumber": [primary_contact.phoneNumber],
+                "secondaryContactIds": [],
+                }
             }
-        }
-        return JsonResponse(response_data, status=200)
+            return JsonResponse(response_data, status=200)
+
+        else:
+            primary_contact_combined = list(set(primary_contact_phone+primary_contact_email))
+
+            print(primary_contact_combined)
+
+            primary_contact_combined.sort(key=lambda x: x.createdAt)
+
+            primary_contact_old = primary_contact_combined[0]
+            
+            primary_contact_old.linkPrecedence = 'primary'
+            primary_contact_old.save()
+
+            print("primary linkprec updated")
+
+            emailList = [primary_contact_old.email]
+            phoneList = [primary_contact_old.phoneNumber]
+            secondary_contacts=[]
+
+            for i in range(1, len(primary_contact_combined)):
+                secondary_contact = primary_contact_combined[i]
+                secondary_contact.linkPrecedence = 'secondary'
+                secondary_contact.linkedId = primary_contact_old
+                secondary_contact.save()
+                print("sec linkprec updated")
+                if len(primary_contact_email)==0:
+                    emailList.append(secondary_contact.email)
+                if len(primary_contact_phone)==0:
+                    phoneList.append(secondary_contact.phoneNumber)
+                secondary_contacts.append(secondary_contact.id)
+            print("71",secondary_contacts)
+            new_contact = Contact.objects.create(email=email, phoneNumber=phoneNumber, linkPrecedence='secondary', linkedId=primary_contact_old)
+            print("after")
+            if len(primary_contact_email)==0:
+                emailList.append(new_contact.email)
+            if len(primary_contact_phone)==0:
+                phoneList.append(new_contact.phoneNumber)
+            secondary_contacts.append(new_contact.id)
+            print("79",secondary_contacts)
+
+            response_data = {
+            "contact": {
+                "primaryContactId": primary_contact_old.id,
+                "emails": emailList,
+                "phoneNumber": phoneList,
+                "secondaryContactIds": secondary_contacts,
+                }
+            }
+            return JsonResponse(response_data, status=200)
+              
+
+        # if phoneNumber and primary_contact.linkPrecedence=='primary':
+        #     primary_contact.phoneNumber = phoneNumber
+        #     primary_contact.save()
+
+
+        # linkedContacts = []
+        # emailList = []
+        # phoneList = []
+        # secondaryContacts = []
+
+        # linkedContacts = Contact.objects.filter(linkedId = primary_contact.id)
+
+        # emailList.append(primary_contact.email)
+        # phoneList.append(primary_contact.phoneNumber)
+
+        # for contact in linkedContacts:
+        #     if contact.linkPrecedence == 'secondary':
+        #         emailList.append(contact.email)
+        #         phoneList.append(contact.phoneNumber)
+        #         secondaryContacts.append(contact.id)
+
+
+        # if primary_contact.linkPrecedence == 'primary':
+        #     primary_contact.linkPrecedence = 'secondary'
+        #     primary_contact.save()
+        
+        # secondary_contact = Contact.objects.create(email=email, phoneNumber=phoneNumber, linkPrecedence='secondary', linkedId = primary_contact)
+
+
+        
     
     return JsonResponse({"message":"Invalid information"}, status=405)
-
-
