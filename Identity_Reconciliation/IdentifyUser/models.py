c04684998243be7b781d2daae7ae04783c70b4bf
tawheed78 <119650487+tawheed78@users.noreply.github.com>
2023-07-24 00:48:05 +0530

    Removed extra comments

--- Identity_Reconciliation/IdentifyUser/models.py
+++ Identity_Reconciliation/IdentifyUser/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Contact(models.Model):
     phoneNumber = models.CharField(max_length=15, unique=False, null=True)
     email = models.CharField(max_length=100, unique=False, null=True)
-    # linkedId = models.ForeignKey('self',null=True, blank=True, on_delete=models.SET_NULL)
     linkedId = models.IntegerField(null=True, blank=True)
     linkPrecedence = models.CharField(max_length=10, default='primary')
     createdAt = models.DateTimeField(auto_now_add=True)
