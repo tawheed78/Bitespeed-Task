6f66cda01938c0c38cdc911a4fa3dfdeb86c7156
Tawheed <tawheed.chilwan55@gmail.com>
2023-07-23 22:26:56 +0530

    Added gitignore

--- Identity_Reconciliation/Identity_Reconciliation/settings.py
+++ Identity_Reconciliation/Identity_Reconciliation/settings.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 from pathlib import Path
 
@@ -20,10 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&hm)h++w4&u_n4y3h5vs2@bhe31(k^qvb4tiu*_84dv0ud$mt("
+#SECRET_KEY = "django-insecure-&hm)h++w4&u_n4y3h5vs2@bhe31(k^qvb4tiu*_84dv0ud$mt("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecretkey')
 
 ALLOWED_HOSTS = []
 
